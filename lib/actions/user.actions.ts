1019902ded35ef001fedc9c884f3abd13d1b321b
Avas <avas.bajracharya@renegadeinsurance.com>
2024-02-19 21:13:20 +0545

    feat: added models for category and orders

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,9 @@ import { CreateUserParams, UpdateUserParams } from "@/types";
 import { handleError } from "../utils";
 import { connectToDb } from "../database";
 import User from "../database/models/user.model";
+import Event from "../database/models/event.model";
+import { revalidatePath } from "next/cache";
+import Order from "../database/models/order.model";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -26,4 +29,35 @@ export const updateUser = async (clerkId: string, user: UpdateUserParams) => {
   } catch (error) {
     handleError(error);
   }
-};  
+};
+
+export const deleteUser = async (clerkId: string) => {
+  try {
+    await connectToDb();
+    const userToDelete = await User.findOne({ clerkId });
+    if (!userToDelete) {
+      throw new Error("User not found");
+    }
+    await Promise.all([
+      // Update the 'events' collection to remove references to the user
+      Event.updateMany(
+        { _id: { $in: userToDelete.events } },
+        { $pull: { organizer: userToDelete._id } }
+      ),
+
+      // Update the 'orders' collection to remove references to the user
+      Order.updateMany(
+        { _id: { $in: userToDelete.orders } },
+        { $unset: { buyer: 1 } }
+      ),
+    ]);
+
+    // Delete user
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+    revalidatePath("/");
+
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+  } catch (error) {
+    handleError(error);
+  }
+};
