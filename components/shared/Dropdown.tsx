ba9cd48955956f9fe809d6fab3a4e87c6108e123
Avas <avas.bajracharya@renegadeinsurance.com>
2024-02-20 18:21:17 +0545

    feat: created event schema and form

---
+++ components/shared/Dropdown.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Input } from "../ui/input";
+
+const Dropdown = () => {
+  return <div></div>;
+};
+
+export default Dropdown;
